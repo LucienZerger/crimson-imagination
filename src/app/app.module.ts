aafc246ef5d782320c1424c3aeaee13d7a41334f
dieter <lucien.zerger@gmail.com>
2017-05-30 19:12:12 +1000

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// components
 import { AppComponent } from './components/app/app.component';
 import { CrimsonTitleComponent } from './components/crimson-title/crimson-title.component';
+
+// services
+import { UserService } from './services/user.service';
+import { PhotoService } from './services/photo.service';
+
 
 @NgModule({
   declarations: [
